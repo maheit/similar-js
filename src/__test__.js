bdd7d70632f3204e200e10c6c53371ccc14162f8
MaheIT <maheitdev@gmail.com>
2024-09-17 11:04:29 +0530

    build script changes

--- src/__test__.js
+++ src/__test__.js
@@ -172,3 +172,51 @@ console.log(isSimilar(newUserDetails, previousUserDetails));
 console.log(isSimilar(10, 10))
 // console.log(isSimilar.isSimilar(10, 10));
 //   console.log(similar(10, 10));
+
+// README.md conditions
+console.log("-----------README.md-----------------")
+
+console.log(isSimilar("str", "int"));
+console.log(isSimilar("string", "string"));
+console.log(isSimilar("str", 10));
+console.log(isSimilar(10, 10));
+console.log(isSimilar({}, null));
+console.log(isSimilar({ env: "dev" }, { env: "dev" }));
+console.log(isSimilar({ env: "dev" }, { env: "prod", "debug": false }));
+console.log(isSimilar(undefined, [1]));
+console.log(isSimilar([10, 20, 300], [20, 10]))
+console.log(isSimilar([10, 20, 300], [20, 10, 40]))
+
+
+console.log("--- meta ---")
+
+console.log(isSimilar([1, 2, 3], [2, 1], { exactArray: false })); // true, because all the items in minimum length (second argument) array present in another array (first argument).
+
+console.log(isSimilar([1, 2, 3], [2, 1], { exactArray: true })); // false, because all the items in minimum length (second argument) array present in another array (first argument). since meta option exactArray: true ensure that array should have same no.of item in both array and all of them should be equest may be order of elem may be changes
+
+console.log(isSimilar([1, 2, 3], [2, 1], { exactArray: false, swapCompArray: false })); // false, because all the items in minimum length (second argument) array present in another array (first argument). since meta option swapCompArray: false ensure that base array should be the first argument itself so items present in first arg was not present in second argument
+
+console.log(isSimilar([11, 3], [21, 1, 3, 11], { swapCompArray: false, exactArray: false })); // true, because all the items in first argument (which has lesser items in array, lesser array item will takes as base array for comparision) array present in second argument array.
+
+console.log(isSimilar([21, 1, 3, 11], [11, 3], { swapCompArray: false, exactArray: false })); // false, because all the items in first argument array (because swapCompArray: false, so first argument always taken for base comparator) are not present in second argument array.
+
+console.log(isSimilar({ name: "mahe" }, {}, { exactObject: false, swapCompObject: false })); // false, because second argument object atleast should include all the property of first argument object.
+
+console.log(isSimilar({ name: "mahe" }, {}, { exactObject: false })); // true, since swap of argument possible due to default swapComparisonObject as true so second argument has the minimum property than first then it means another object (first argument) has all the property of seconf argument object so it return true.
+
+console.log(isSimilar({ name: "mahe", school: "ICSA", "class": "10" }, { name: "mahe", school: "ICSA" }, { exactObject: true })); // false
+
+
+console.log(isSimilar({ env: "dev" }, { env: "dev", "debug": true }, { exactObject: false })); //  { exactObject: false }
+
+console.log(isSimilar([10, 20, 300], [20, 10], { exactArray: false })) // {exactArray: false}
+
+console.log(isSimilar({ name: "mahe", school: "ICSA", "class": "10" }, { name: "mahe", school: "ICSA", "class": "10" })); // true, default meta options value is { exactObject: true,exactArray: true,swapCompObject: true,swapCompArray: true }
+
+console.log(isSimilar({ name: "mahe", school: "ICSA", "class": "10" }, { name: "mahe", "class": "10" })); // true, default meta options value is { exactObject: true, exactArray: true, swapCompObject: true, swapCompArray: true }
+
+
+console.log("----deep comparision-----")
+
+
+console.log(isSimilar(newUserDetails, previousUserDetails));
